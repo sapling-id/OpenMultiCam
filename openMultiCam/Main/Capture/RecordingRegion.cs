e0b24b65b3633a65a70788e039574a5a6892ec66
Vinz <v.baeumann@gmail.com>
2018-01-23 09:57:02 +0100

    initial commit

---
+++ openMultiCam/Main/Capture/RecordingRegion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openMultiCam.Utils {
+    public interface RecordingRegion {
+
+        void getWidth(ScreenCaptureUtilities screenCaptureUtils);
+        void getHeight(ScreenCaptureUtilities screenCaptureUtils);
+        void getOffestX(ScreenCaptureUtilities screenCaptureUtils);
+        void getOffsetY(ScreenCaptureUtilities screenCaptureUtils);
+    }
+}
---
+++ openMultiCam/Models/VideoPreviewItemModel.cs
@@ -0,0 +1,50 @@
+﻿using openMultiCam.WorkspaceUtils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace openMultiCam.GuiCustoms {
+    public class VideoPreviewItemModel: INotifyPropertyChanged {
+
+        public VideoPreview ownVideoPreviewInstance { get; set; }
+        public RecordingGallery ParentWindow { get; set; }
+        private ImageSource PrevieImage_ { get; set; }
+        public ImageSource PreviewImage {
+            get {
+                return PrevieImage_;
+            }
+            set {
+                PrevieImage_ = value;
+                OnPropertyChanged("PreviewImage");
+            }
+        }
+        public String PlayTime { get; set; }
+        public String RecordingQuality { get; set; }
+        private System.Windows.Media.Brush ForegroundColor_ { get; set; }
+        public System.Windows.Media.Brush ForegroundColor
+        {
+            get { return ForegroundColor_; }
+            set
+            {
+                ForegroundColor_ = value;
+                OnPropertyChanged("ForegroundColor");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name) {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
